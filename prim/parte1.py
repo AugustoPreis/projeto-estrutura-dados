37dc7878126276482d690f0ecdf7d8c1cf2b90b7
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-27 14:54:09 -0300

    adiciona barra de progresso e tabela de resultados na parte 1

--- prim/parte1.py
+++ prim/parte1.py
@@ -1,5 +1,9 @@
+import utils.console as console
+
 from utils.generateArray import generateArray
 from utils.calculateTime import calculateTime
+from utils.progressBar import ProgressBar
+from utils.table import table
 
 from sorters.bubbleSort import bubbleSort
 from sorters.insertionSort import insertionSort
@@ -7,13 +11,29 @@ from sorters.mergeSort import mergeSort
 from sorters.quickSort import quickSort
 
 array = generateArray(10000)
+progressBar = ProgressBar()
 
+progressBar.update(0, 'Executando bubble sort')
 bubbleSortResult, bubbleSortTime = calculateTime(bubbleSort, array)
+
+progressBar.update(25, 'Executando insertion sort')
 insertionSortResult, insertionSortTime = calculateTime(insertionSort, array)
+
+progressBar.update(50, 'Executando merge Sort')
 mergeSortResult, mergeSortTime = calculateTime(mergeSort, array)
+
+progressBar.update(75, 'Executando quick Sort')
 quickSortResult, quickSortTime = calculateTime(quickSort, array)
 
-print(f'Bubble Sort: {bubbleSortTime}')
-print(f'Insertion Sort: {insertionSortTime}')
-print(f'Merge Sort: {mergeSortTime}')
-print(f'Quick Sort: {quickSortTime}')
+progressBar.end()
+console.clear()
+
+table(
+  ['Algoritmo', 'Tempo de execução'],
+  [
+    ['Bubble Sort', bubbleSortTime],
+    ['Insertion Sort', insertionSortTime],
+    ['Merge Sort', mergeSortTime],
+    ['Quick Sort', quickSortTime],
+  ],
+)
